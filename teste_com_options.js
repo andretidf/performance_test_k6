7e9081b457b6b15109ae04e453926570f6455ff3
Andre Muniz <andretidf@gmail.com>
2022-08-30 11:45:25 -0300

    first test with k6

---
+++ teste_com_options.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const url = 'http://localhost:80';
+
+export const options = {
+    vus: 10,
+    duration: '30s',
+};
+
+export default function(){
+    http.get(url);
+    sleep(1);
+}
